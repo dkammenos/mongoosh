9d0f38a9b2527dd2f0516c7fbc3f8f075597da01
ΔΙΟΝΥΣΙΟΣ ΚΑΜΜΕΝΟΣ <std080228@ac.eap.gr>
2022-12-23 12:43:06 +0200

    updated app

--- app.js
+++ app.js
@@ -89,6 +89,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+app.listen(process.env.PORT || 3000, () => {
+  console.log("O Server τρέχει στην πόρτα 3000");
+  }); 
